2e41e9ba11e4926b508af490c128acdbee46238b
TUYIZERE Samuel <sam@ejobu.rw>
2019-06-25 14:37:19 +0200

    ready for heroku

--- farmcomm/settings.py
+++ farmcomm/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'web',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
@@ -121,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+
+)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
